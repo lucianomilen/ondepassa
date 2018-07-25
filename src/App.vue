6326094019a9f70946dbca480383106d4558c147
Delisson <delisson@britecore.com>
2018-07-24 21:37:21 -0300

    Removendo quasar e adicionando element-ui
    Signed-off-by: Delisson <delisson@britecore.com>

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,30 @@
 <template>
-  <div id="q-app">
-    <router-view />
+  <div id="app">
+    <img src="./assets/logo.png">
+    <div>
+      <el-button @click="startHacking">Start</el-button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  methods: {
+    startHacking () {
+      this.$notify({
+        title: 'It works!',
+        type: 'success',
+        message: 'We\'ve laid the ground work for you. It\'s time for you to build something epic!',
+        duration: 5000
+      })
+    }
+  }
 }
 </script>
 
 <style>
+#app {
+  font-family: Helvetica, sans-serif;
+  text-align: center;
+}
 </style>
